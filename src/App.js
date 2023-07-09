76b68bbc66670f7cca15e21f2f119e7b09b0e306
James Parrish <jthomasparrishjr@gmail.com>
2023-07-09 18:28:18 -0500

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <div>
-      <h1>Jimmy Parrish</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+function NavBar() {
+  const [nav, setNav] = useState(false);
+
+  function handClick() {
+    setNav(!nav);
+  }
+  return (
+    <div className="fixed w-full h-[80px] flex justify-end items-center px-4 bg-[#0a192f] text-gray-300">
+      {/* Menu */}
+
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Hamburger */}
+      <div className="md:hidden z-10" onClick={handClick}>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile Menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Projects</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
+      {/* Social Icons */}
+      <div className="hidden"></div>
+    </div>
+  );
+}
+
+export default NavBar;
