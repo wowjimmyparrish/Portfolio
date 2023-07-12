2c88288ebcf0028802d5e7a81747ba76f33ca988
James Parrish <jthomasparrishjr@gmail.com>
2023-07-11 21:51:52 -0500

    added projects component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 function App() {
   return (
     <div>
@@ -9,6 +10,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import BjjHub from "../assets/bjjhub.png";
+import WorkoutNow from "../assets/workoutnow.png";
+
+function Projects() {
+  return (
+    <div
+      name="projects"
+      className="w-full md:h-screen text-gray-300 bg-[#0a192f]"
+    >
+      <div className="max-w-[1000px] mx-auto px-4 flex flex-col justify-center h-full w-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-300 text-gray-300">
+            Projects
+          </p>
+          <p className="py-4">// Check out some of my recent projects</p>
+        </div>
+
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid-Item */}
+          <div
+            style={{ backgroundImage: `url(${BjjHub})` }}
+            className="shadow-lg shadow-black group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 ">
+              <span className="text-2xl font bold text-white tracking-wider ">
+                {" "}
+                React/Rails Application
+              </span>
+              <div className="pt-8 text-center ">
+                <a href="https://bjjhub-app.onrender.com">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Site
+                  </button>
+                </a>
+                <a href="https://github.com/wowjimmyparrish/bjjhub.git">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${WorkoutNow})` }}
+            className="shadow-lg shadow-black group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 ">
+              <span className="text-2xl font bold text-white tracking-wider ">
+                {" "}
+                React/Rails Application
+              </span>
+              <div className="pt-8 text-center ">
+                <a href="https://workoutnow-app.onrender.com">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Site
+                  </button>
+                </a>
+                <a href="https://github.com/wowjimmyparrish/workoutnow.git">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
