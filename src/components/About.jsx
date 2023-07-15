033808251721f891c7d0d525c8ae3964be92253e
James Parrish <jthomasparrishjr@gmail.com>
2023-07-15 16:35:17 -0500

    updated container height

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 function About() {
   return (
-    <div name="about" className="w-full h-screen  bg-[#0a192f] text-gray-300">
+    <div
+      name="about"
+      className="w-full min-h-screen  bg-[#0a192f] text-gray-300"
+    >
       <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="max-w-[1000px] w-full grid grid-cols-1 sm:grid-cols-2 gap-8">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="text-center sm:text-right pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-gray-300">
               About
