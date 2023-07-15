4adcbb8b079965dc3741ee7a78dba6ff2cb36506
James Parrish <jthomasparrishjr@gmail.com>
2023-07-15 16:47:58 -0500

    updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,18 +2,50 @@ import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { Link } from "react-scroll";
 
+// function Home() {
+//   return (
+//     <div name="home" className="w-full h-screen bg-[#0a192f]">
+//       {/* Container */}
+//       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+//         <h1 className="text 4xl sm:text-7xl font-bold text-[#ccd6f6] ">
+//           James Parrish
+//         </h1>
+//         <h2 className="text 4xl sm:text-7xl font-bold text-[#8892b0]">
+//           Full-Stack Software Engineer
+//         </h2>
+//         <p className="text-[#8892b0] py-4 max-w-[700px]">
+//           I'm a full-stack software engineer with a passion for creating
+//           efficient and effective code.
+//         </p>
+//         <div>
+//           <button className="text-white group border-2 px-6 py-2 my-2 flex items-center hover:border-[#8892b0] hover:text-[#8892b0]">
+//             <Link to="projects" smooth={true} duration={500}>
+//               View Projects
+//             </Link>
+//             <span className="group-hover:rotate-90 duration-300">
+//               <HiArrowNarrowRight className="ml-3" />
+//             </span>
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
 function Home() {
   return (
-    <div name="home" className="w-full h-screen bg-[#0a192f]">
+    <div name="home" className="w-full h-screen bg-[#0a192f] flex items-center">
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <h1 className="text 4xl sm:text-7xl font-bold text-[#ccd6f6] ">
+      <div className="max-w-[1000px] mx-auto px-8">
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6] mb-4 sm:mb-8">
           James Parrish
         </h1>
-        <h2 className="text 4xl sm:text-7xl font-bold text-[#8892b0]">
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0] mb-4 sm:mb-8">
           Full-Stack Software Engineer
         </h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">
+        <p className="text-[#8892b0] max-w-[700px] mb-8">
           I'm a full-stack software engineer with a passion for creating
           efficient and effective code.
         </p>
